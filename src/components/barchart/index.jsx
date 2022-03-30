a6767370233a9639e8798c06d3ed8a0e7bb50519
StrikerOne65 <golamrabbani20014@gmail.com>
2022-03-31 01:35:36 +0530

    Added chart js

---
+++ src/components/barchart/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Bar, Chart } from 'react-chartjs-2'
+
+export default function Barchart({ data }) {
+  const [barData, setBarData] = useState({
+    labels: data.map((item) => item.teacherName),
+    datasets: [
+      {
+        label: 'Average Rating out of 37',
+        data: data.map((item) => item.avgRating),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(153, 102, 255, 0.5)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132)',
+          'rgba(75, 192, 192)',
+          'rgba(153, 102, 255)',
+        ],
+        borderWidth: 2,
+        hoverBorderColor: 'black',
+      },
+    ],
+  })
+
+  const options = {
+    layout: {
+      padding: 20,
+    },
+  }
+  return (
+    <div className='chartBg'>
+      <Bar data={barData} options={options} />
+    </div>
+  )
+}
